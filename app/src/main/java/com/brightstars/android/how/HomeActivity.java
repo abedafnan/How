b2e2a58006dd5182cba6bf2f999416b98c4edeba
Afnan A. Abed <haeseongp@gmail.com>
2018-09-23 21:08:31 +0300

    Edit the home activity

--- app/src/main/java/com/brightstars/android/how/HomeActivity.java
+++ app/src/main/java/com/brightstars/android/how/HomeActivity.java
@@ -7,6 +7,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.firebase.iid.FirebaseInstanceId;
@@ -20,17 +22,28 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.home_toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.search_fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Setting the title of the toolbar
+        getSupportActionBar().setTitle("Home");
 
         // Will be sent to server with the user's register info
         Log.d("TOKEN", FirebaseInstanceId.getInstance().getToken());
+    }
+
+    // Inflating the option menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_search) {
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
