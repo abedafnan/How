32a835d038057bbb94f6dfd2a9ab977491673062
Afnan A. Abed <haeseongp@gmail.com>
2018-09-24 11:28:54 +0300

    Prepare the bottom navigation

--- app/src/main/java/com/brightstars/android/how/HomeActivity.java
+++ app/src/main/java/com/brightstars/android/how/HomeActivity.java
@@ -2,31 +2,55 @@ package com.brightstars.android.how;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.home_toolbar);
         setSupportActionBar(toolbar);
 
-        // Setting the title of the toolbar
-        getSupportActionBar().setTitle("Home");
-
         // Will be sent to server with the user's register info
         Log.d("TOKEN", FirebaseInstanceId.getInstance().getToken());
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
     // Inflating the option menu
---
+++ app/src/main/java/com/brightstars/android/how/fragments/HomeFragment.java
@@ -0,0 +1,8 @@
+package com.brightstars.android.how.fragments;
+
+/**
+ * Created by Afnan A. A. Abed on 9/24/2018.
+ */
+
+public class HomeFragment {
+}
