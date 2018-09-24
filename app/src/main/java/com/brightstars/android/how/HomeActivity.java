fc18a0bd12fbd124733eff54daa3fb18fa827df9
Afnan A. Abed <haeseongp@gmail.com>
2018-09-24 12:05:33 +0300

    Make small edits on main and home activities

--- app/src/main/java/com/brightstars/android/how/HomeActivity.java
+++ app/src/main/java/com/brightstars/android/how/HomeActivity.java
@@ -15,8 +15,6 @@ import com.google.firebase.iid.FirebaseInstanceId;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -24,13 +22,13 @@ public class HomeActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+
                     return true;
             }
             return false;
@@ -48,7 +46,6 @@ public class HomeActivity extends AppCompatActivity {
         // Will be sent to server with the user's register info
         Log.d("TOKEN", FirebaseInstanceId.getInstance().getToken());
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
