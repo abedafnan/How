c7bb875a982cf7a9b744266ad2b22d3d1dbd4614
Afnan A. Abed <haeseongp@gmail.com>
2018-09-26 18:32:17 +0300

    Prepare the settigns screen

--- app/src/main/java/com/brightstars/android/how/SettingsActivity.java
+++ app/src/main/java/com/brightstars/android/how/SettingsActivity.java
@@ -1,28 +1,42 @@
 package com.brightstars.android.how;
 
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.LinearLayout;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatPreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
 
-        Toolbar toolbar = findViewById(R.id.settings_toolbar);
+        Toolbar toolbar = new Toolbar(this);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, R.attr.actionBarSize);
+        toolbar.setLayoutParams(layoutParams);
+        toolbar.setPopupTheme(R.style.AppTheme);
+        toolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
+//        Toolbar toolbar = findViewById(R.id.settings_toolbar);
         setSupportActionBar(toolbar);
         // Displaying the back arrow
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         // Setting the title of the toolbar
         getSupportActionBar().setTitle("Settings");
+
+        // load settings fragment
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MainPreferenceFragment()).commit();
     }
 
-    // Code for the back arrow
-    @Override
-    public boolean onSupportNavigateUp() {
-        onBackPressed();
-        return true;
+    public static class MainPreferenceFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+        }
     }
 }
