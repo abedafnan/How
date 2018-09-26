b36c58a6d3c464411121650bb108c1d6f6b7d67c
Afnan A. Abed <haeseongp@gmail.com>
2018-09-26 19:32:26 +0300

    Small fixes and changes

--- app/src/main/java/com/brightstars/android/how/SettingsActivity.java
+++ app/src/main/java/com/brightstars/android/how/SettingsActivity.java
@@ -2,9 +2,13 @@ package com.brightstars.android.how;
 
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 public class SettingsActivity extends AppCompatPreferenceActivity {
@@ -12,20 +16,8 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Toolbar toolbar = new Toolbar(this);
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT, R.attr.actionBarSize);
-        toolbar.setLayoutParams(layoutParams);
-        toolbar.setPopupTheme(R.style.AppTheme);
-        toolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-
-//        Toolbar toolbar = findViewById(R.id.settings_toolbar);
-        setSupportActionBar(toolbar);
-        // Displaying the back arrow
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        // Setting the title of the toolbar
-        getSupportActionBar().setTitle("Settings");
+        // Inflate the toolbar
+        setupActionBar();
 
         // load settings fragment
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MainPreferenceFragment()).commit();
@@ -38,5 +30,34 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
         }
+    }
+
+    private void setupActionBar() {
+        ViewGroup rootView = (ViewGroup)findViewById(R.id.action_bar_root); //id from appcompat
+
+        if (rootView != null) {
+            View view = getLayoutInflater().inflate(R.layout.app_bar_layout, rootView, false);
+            rootView.addView(view, 0);
+
+            Toolbar toolbar = (Toolbar)findViewById(R.id.settings_toolbar);
+            setSupportActionBar(toolbar);
+        }
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("Settings");
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return false;
     }
 }
